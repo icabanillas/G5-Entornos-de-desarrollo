b1303ef00d352d9041cc8f71f644ce36ce4d0882
inteltg <teamgamesjuegos@gmail.com>
2026-03-10 22:23:01 +0100

    Rediseño de los métodos y código
    Cambios en el código de GestionUsuarios.

--- Metodos.java
+++ Metodos.java
@@ -5,15 +5,14 @@
 package Ent_Des;
 
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  *
  * @author inteltg
  */
-public class Metodos {
-
-    public List<Usuario> usuarios = new ArrayList<>();
+public class GestionUsuarios {
+    //Cambio el List por un ArrayList, lo hago para no tener que estar importando tanto List como ArrayList
+    public ArrayList<Usuario> usuarios = new ArrayList<>();
     
     /**
      * 
@@ -26,25 +25,12 @@ public class Metodos {
         try { //intentamos crear el usuario
             Usuario user = new Usuario(nombre, email, password, edad);
             usuarios.add(user); //si el usuario es creado exictosamente se guarda
-
-            enviarEmail(usuarios, edad); //metodo para enviar el email si cumple con mayoria de edad
+            
         } catch (Error_Des e) { //si algun dato falla, no se crea al usuario y se muestra el primer error
             System.out.println(e);
         }
     }
     
-    /**
-     * 
-     * @param data
-     * @param edad 
-     */
-    public void enviarEmail(List<Usuario> data, int edad) {
-        if (edad >= 18) { //si la edad es 18 o mas, simula el envio de un email
-            System.out.println("Enviando correo a " + data.getLast());
-        } else { //si no es mayor de edad, se informa al usuario que es menor de edad
-            System.out.println("El usuario es demasiado joven (-18)");
-        }
-    }
     
     /**
      * 
@@ -52,14 +38,14 @@ public class Metodos {
      * @param nombre
      * @param nuevoEmail 
      */
-    public void actualizarEmail(List<Usuario> data, String nombre, String nuevoEmail) {
+    public void actualizarEmail(ArrayList<Usuario> data, String nombre, String nuevoEmail) {
         boolean encontrado = false;
-        if (!data.isEmpty()) { //si la lista de usuarios esta vacio se informa si no continua la busqueda del usuario
+        if (!data.isEmpty()) { //si la lista de usuarios esta vacio se informa si no, se continua la busqueda del usuario
             for (int ini = 0; ini < data.size() && !encontrado; ini++) { //for para recorrer la lista, solo la recorre si hay un next y no se ha encontrado al usuario
                 Usuario user = data.get(ini);
                 if (user.getNombre().equalsIgnoreCase(nombre)) { //si el nombre coincide (ignorando mayusculsa y minusculas)
                     try { //intentamos el cambio de email
-                        user.setEmail(nuevoEmail);
+                        user.setEmail(nuevoEmail); //usamos el setter de email que tiene dentro del mismo el validador de email
                         encontrado = true; //informamos que ya lo hemos encontrado para terminar el for
                     } catch (Error_Des e) { //si da error, atrapamos la excepcion
                         System.out.println(e); //mostramos por pantalla cual ha sido el problema
@@ -71,4 +57,3 @@ public class Metodos {
         }
     }
 }
-
